c8ea014c34437969876471297cd3b86e3ea76b60
raccooncho <wjtjdrb@naver.com>
2019-02-24 13:12:02 +0900

    delete 1 directory

--- test2.py
+++ test2.py
@@ -3,37 +3,21 @@ sys.stdin = open('input.txt' ,'r')
 
 T = int(input())
 for t in range(T):
-    rounds = int(input())
-    num_set = []
-    for r in range(rounds):
-        dum = list(map(int, input().split()))
-        num_set += [dum]
-    roads = len(num_set) * 2 - 2
-    row = len(num_set)
-    min_roads = 0
-    for i in range(1 << roads):
-        if str(bin(i)).count('1') == (roads >> 1):
-            if str(bin((1 << roads) - 1 - i)).count('1') == (roads >> 1):
-                x = 0
-                y = 0
-                deft = num_set[y][x]
-                road_sum = deft
-                cntx = 0
-                cnty = 0
-                bp = 0
-                for j in range(roads):
-                    if i & (1 << j):
-                        if cntx < len(num_set) - 1:
-                            x += 1
-                            cntx += 1
-                    else:
-                        if cnty < len(num_set) - 1:
-                            y += 1
-                            cnty += 1
-                    deft = num_set[y][x]
-                    road_sum += deft
-                if min_roads == 0:
-                    min_roads = road_sum
-                elif road_sum < min_roads:
-                    min_roads = road_sum
-    print(f'#{t + 1} {min_roads}')
+    rounds = list(map(int, input().split()))
+    bus = rounds[0]
+    busstop = rounds[1:bus] + [0]
+
+    cnt = 0
+    roads = 0
+    fuel = 0
+    while roads < bus - 1:
+        if bus - 1 - roads <= busstop[fuel] + fuel:
+            cnt += 1
+            roads = bus - fuel - 1
+            fuel = 0
+        else:
+            fuel += 1
+
+
+
+    print(f'#{t + 1} {cnt-1}')
